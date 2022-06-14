3aecc9e86ea757fff890f55d63525b3dee82a7a9
Jefrin Jojan <jefrin@fb.com>
2022-06-14 13:29:23 -0700

    Added MainActivity UI for capturing and sunmbiting posts -1st part of using Parse to submit user posts

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "Logout";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
+
+    }
+
+    private void onLogout() {
+        ParseUser.logOutInBackground();
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        Toast.makeText(this, "You been logged out", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.mLogout){
+           onLogout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/instagram/PraseApplication.java
+++ app/src/main/java/com/example/instagram/PraseApplication.java
@@ -3,12 +3,16 @@ package com.example.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class PraseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("Inal0ODHrPqFV4IxrU7qDxbHtuJyGl5fAP9paJ8q")
                 .clientKey("2DRdTXr6JOpK3kcd59gebkBqBWv6xpKd4Tsu3FPo")
