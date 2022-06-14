96ee94d839a5d22af8aa975b976030ad1c974589
Jefrin Jojan <jefrin@fb.com>
2022-06-14 13:49:49 -0700

    Added MainActivity UI for capturing and sunmbiting posts -1st part of using Parse to submit user posts

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -12,7 +12,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+            if(e != null){
+                Log.e(TAG, "ERROR with getting posts", e);
+                return;
+            }
+                for (Post post: posts){
+                    Log.i(TAG, "description: "+ post.getDescription() + " username " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 
     private void onLogout() {
